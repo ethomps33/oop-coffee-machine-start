b9daaa5f20eddc2230ba7dc7f7cf853887646a55
ethomps33 <e.thomps33@gmail.com>
2023-07-13 23:11:52 -0700

    Created a function that runs all the code to get coffee. Trying to get the wallet to subtract money

--- main.py
+++ main.py
@@ -10,26 +10,35 @@ latte = MenuItem(name="latte", cost=2.5, water=200, milk=150, coffee=24)
 espresso = MenuItem(name="espresso", water=50, milk=0, coffee=18, cost=1.5)
 cappuccino = MenuItem(name="cappuccino", water=250, milk=50, coffee=24, cost=3)
 
-machine.report()
 
-money.report()
+def get_coffee():
+    machine.report()
 
-order = input(f"Hi! What can I get for you today? {menu.get_items()}: ").lower()
+    money.report()
 
-menu.find_drink(order)
-if order == "latte":
-    order = latte
-elif order == "espresso":
-    order = espresso
-elif order == "cappuccino":
-    order = cappuccino
+    order = input(f"Hi! What can I get for you today? {menu.get_items()}: ").lower()
 
-machine.is_resource_sufficient(order)
+    menu.find_drink(order)
+    if order == "latte":
+        order = latte
+    elif order == "espresso":
+        order = espresso
+    elif order == "cappuccino":
+        order = cappuccino
 
-money.make_payment(order.cost)
+    machine.is_resource_sufficient(order)
 
-if money.make_payment(order.cost):
-    machine.make_coffee(order)
+    enough = money.make_payment(order.cost)
+
+    if enough:
+        machine.make_coffee(order)
+
+    money.report()
+    addtl = input("Do you want to order another drink? Yes or No: ").lower()
+
+    if addtl == 'yes':
+        get_coffee()
 
 
+get_coffee()
 
